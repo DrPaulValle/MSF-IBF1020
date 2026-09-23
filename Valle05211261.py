cc326135e7397fff4c58194a5764d38fd9bb8655
Paul Uchiha <paul.valle@tectijuana.edu.mx>
2026-09-22 19:34:15 -0700

    Add files via upload

---
+++ Valle05211261.py
@@ -0,0 +1,15 @@
+"""
+Práctica 0: Mecánica pulmonar
+
+Tecnológico Nacional de México - Instituto Tecnológico de Tijuana
+Departamento de Ingeniería Eléctrica y Electrónica
+Carrera: Ingeniería Biomédica
+Asignatura: Modelado de Sistemas Fisiológicos
+Docente: Dr. Paul Antonio Valle Trujillo; paul.vt@tijuana.tecnm.mx
+
+Nombre del alumno: Nombres y Apellidos
+Número de control: 12345678
+Correo institucional: xxxxxx@tijuana.tecnm.mx
+"""
+# Instalar librerias en consola: pip install control slycot matplotlib
+
